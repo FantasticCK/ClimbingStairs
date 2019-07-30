72a7929022c0ddbc16c02b12db1a8d9ae0fce521
CK <kainingc@usc.edu>
2019-07-30 15:20:51 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,25 @@
+package com.CK;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 1;
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(n));
+    }
+}
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
